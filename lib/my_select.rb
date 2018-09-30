cee8572a26e87562740bec8bc28e10f0fc38cefd
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-30 00:04:48 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
  # your code here!
+i =0 
+alteredcollection =[]
+while i< collection.length 
+
+if yield(collection[i]) == true 
+alteredcollection << collection[i]
 end
+i += 1 
+end 
+alteredcollection
+end 
+ 
+
